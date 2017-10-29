77211c1e5f3e0bae4e2ad70c7d30572a2d9fe909
michaelpurnell <michael@sievent.com>
2017-10-28 19:21:21 -0700

    added example of request stream

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 'use strict'
 var https = require('https'),
 tar = require('tar'),
-zlib = require('zlib');
+zlib = require('zlib'),
+request = require('request'),
+fs = require('fs'),
+unzip = require('unzip');
 
 function downloadPackages (count, callback) {
 
+	var url = 'https://registry.npmjs.org/lodash/-/lodash-4.17.4.tgz';
 	console.log("reading");
 
-	var req = https.get('https://registry.npmjs.org/lodash/-/lodash-4.17.4.tgz');
-	req.on('response', function(res) {
-    res
-        .pipe(zlib.createGunzip())
-        .pipe(tar.x({path:'/.packages/', strip: 1}))
-});
+// 	var req = https.get(url);
+// 	req.on('response', function(res) {
+//     res
+//         .pipe(zlib.createGunzip())
+//         .pipe(tar.x({path:'./packages/', strip: 1}))
+// });
+
+//creates the tgz file but doesent extract contents
+request(url).pipe(fs.createWriteStream('./packages/lodash.tgz'));
+
 }
 
 module.exports.downloadPackages = downloadPackages
