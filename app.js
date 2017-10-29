14103eacb56d32351c64fb5c33a02cd145cbf48e
michaelpurnell <michael@sievent.com>
2017-10-28 18:54:09 -0700

    modified to save code just in case the tar files overwrite.

---
+++ app.js
@@ -0,0 +1,8 @@
+var msg = require('./index.js');
+var https = require('https');
+var fs = require('fs');
+var url = require('url');
+
+//console.log(msg.downloadPackages());
+
+msg.downloadPackages()
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 'use strict'
-
-module.exports = downloadPackages
+var https = require('https'),
+tar = require('tar'),
+zlib = require('zlib');
 
 function downloadPackages (count, callback) {
 
+	console.log("reading");
+
+	var req = http.get('https://registry.npmjs.org/lodash/-/lodash-4.17.4.tgz');
+	req.on('response', function(res) {
+    res
+        .pipe(zlib.createGunzip())
+        .pipe(tar.x({path:'/tmp', strip: 1}))
+});
 }
+
+module.exports.downloadPackages = downloadPackages
